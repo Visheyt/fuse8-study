0406b181ee52f13788eb5cf170a2163c6465c8eb
Alex <fornikaras7@gmail.com>
2025-03-26 17:00:03 +0500

    lesson-3 fix stylelint , add command for stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,18 +1,11 @@
 export default {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-clean-order',
+  ],
   plugins: ['stylelint-prettier', 'stylelint-order'],
   rules: {
     'selector-class-pattern': null,
-    'prettier/prettier': true,
   },
-  overrides: [
-    {
-      files: ['**/*.css'],
-      extends: [
-        'stylelint-config-standard',
-        'stylelint-config-standard-scss',
-        'stylelint-config-clean-order',
-      ],
-      rules: {},
-    },
-  ],
 };
